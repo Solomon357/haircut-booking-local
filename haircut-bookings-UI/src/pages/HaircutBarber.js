c430d17c2bbf92674cf6d21e612a5af633d8bbbe
Solomon357 <Solomonoddy@hotmail.com>
2024-03-12 18:29:51 +0000

    moved mock endpoints into seperate folder

--- haircut-bookings-UI/src/pages/HaircutBarber.js
+++ haircut-bookings-UI/src/pages/HaircutBarber.js
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 
 const HaircutBarber = () => {
   const { form, handleChange } = useFormContext();
-  const { allOptions, isLoading, error } = useFetch(`http://localhost:8080/allBarberInfo`);
+  const { allOptions, isLoading, error } = useFetch(`https://raw.githubusercontent.com/Solomon357/haircut-booking-local/main/haircut-bookings-UI/barberdb.json`);
   const navigate = useNavigate();
 
   const styles = {
